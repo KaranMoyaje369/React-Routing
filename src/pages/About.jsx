d7469aca5cd7fc6fd08887d71e20e2f51476b02f
KaranMoyaje369 <karanmoyaje369@gmail.com>
2024-12-17 16:31:52 +0530

    React-Routing-Product-Page

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex justify-center items-center h-screen text-3xl font-bold text-blue-400">
+      About Page
+    </div>
+  );
+};
+
+export default About;
